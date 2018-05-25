004b882fc00adb693f784b01770abfd12ff8f58c
Diana Teresa García Ponce <dgarcia24@ucol.mx>
2018-05-25 16:45:41 -0500

    Add files via upload

---
+++ ColasFIFO/ColasFIFO/Proceso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColasFIFO
+{
+    class Proceso
+    {
+        private int _ciclos;
+        private Proceso _siguiente;
+
+        public int Ciclos { get { return _ciclos; } set { _ciclos = value; } }
+        public Proceso Siguiente { get { return _siguiente; } set { _siguiente = value; } }
+    }
+}
